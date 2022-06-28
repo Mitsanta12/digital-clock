91f185c45e084d87115ecb17174b8f72c0ab4a9a
Mitsanta_ANDRIAH <103644537+Mitsanta12@users.noreply.github.com>
2022-06-28 20:30:23 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+import { Clock, ClockClassComponent } from "./Clock/Clock";
+import Timer from "./setTimer/setTimer";
+function App() {
+  let [position, setPosition] = useState(0);
+  return ( <>
+      {
+        position == 0 ? <Clock /> : <Timer setState={setPosition} state={position} />
+      }
+      <Button state={position} setState={setPosition} />
+    </>
+  );
+}
+
+function Button({ setState, state }) {
+  let label = ["Set Timer", "Start", "Horloge"];
+  return (
+    <button className="btn" onClick={() => setState(state == 2 ? 0 : state + 1)}>{label[state]}</button>
+  );
+}
+
+export default App;
